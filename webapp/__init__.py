b78f93b8c1d1ec6533eece69501891c497bdd12d
Minghao Xu <violiniselegant@gmail.com>
2016-01-19 12:03:45 +0800

    change hierarchy to create and load app

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -2,10 +2,16 @@
 # coding=utf-8
 
 from flask import Flask
+from flask_bootstrap import Bootstrap
 
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'fuck u, you can not guess it'
+    Bootstrap(app)
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'fuck u, you can not guess it'
+    return app
+
+app = create_app()
 
 
 
